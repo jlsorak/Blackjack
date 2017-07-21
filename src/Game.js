004f7dd45954559201bbeb9abeafd79dbbdf7d48
Jean-Luc Sorak <jlsorak@icloud.com>
2017-07-21 16:45:19 +0100

    Hit functionality added

--- src/Game.js
+++ src/Game.js
@@ -15,6 +15,7 @@ class Game extends Component {
     this.placeBet = this.placeBet.bind(this)
     this.getInitialCards = this.getInitialCards.bind(this)
     this.playerHit = this.playerHit.bind(this)
+    this.dealerHit = this.dealerHit.bind(this)
     this.state = {
       gameStarted: false,
       betAmount: 0,
@@ -27,7 +28,6 @@ class Game extends Component {
     this.getInitialCards();
   }
 
-
   startGame() {
     this.setState((prevState, props) => {
       return {
@@ -36,10 +36,17 @@ class Game extends Component {
     })
   }
 
+  placeBet(betAmount) {
+    this.setState((prevState, props) => {
+      return {
+        betAmount
+      }
+    })
+  }
+
   getInitialCards(amount) {
     game.dispatch(actions.deal())
     console.log(game.getState())
-
     this.setState((prevState, props) => {
       return {
         dealerCards: [
@@ -52,58 +59,97 @@ class Game extends Component {
         ]
       }
     })
-
-    return (
-      <div>
-      <button className="modifierButtons">Hit</button>
-      <button className="modifierButtons">Stick</button>
-      </div>
-    )
+    this.checkGameStatus()
   }
 
   playerHit() {
-    
-  }
+    game.dispatch(actions.hit('right'))
 
-  placeBet(betAmount) {
-    this.setState((prevState, props) => {
+    const cards = game.getState().handInfo.right.cards;
+    const playerCards = cards.map(card => {
       return {
-        betAmount
+        number: card.text,
+        suit: card.suite
       }
     })
+    this.setState((prevState,props) => {
+      return {
+        playerCards
+      }
+    })
+    this.checkGameStatus()
   }
 
-  render() {
-    if(this.state.betAmount > 0) {
+  checkGameStatus() {
+    const playerCheck = game.getState().handInfo.right;
+    if (playerCheck.playerHasBlackjack) {
+      console.log('player has blackjack')
+    } else if (playerCheck.playerHasBusted) {
+      console.log('bust!')
+    }
+  }
+
+  dealerHit() {
+    //game.dispatch(actions.dealerHit(game.getState().dealerHoleCard))
+    console.log(game.getState())
+    // const cards = game.getState().handInfo.right.cards;
+    // const playerCards = cards.map(card => {
+    //   return {
+    //     number: card.text,
+    //     suit: card.suite
+    //   }
+    // })
+    // this.setState((prevState,props) => {
+    //   return {
+    //     playerCards
+    //   }
+    // })
+
+
+    //const cards = game.getState()
+    // this.setState((prevState,props) => {
+    //   return {
+    //     dealerCards: state.concat([
+    //       {number: cards.dealerHoleCard.text, suit: cards.dealerHoleCard.suite}
+    //     ])
+    //     }
+    //   })
+    //   console.log(game.getState())
+     }
+
+    render() {
+      if(this.state.betAmount > 0) {
+        return (
+          <div>
+          <Deck cards={this.state.dealerCards} />
+          <Deck cards={this.state.playerCards} />
+          <button className="modifierButtons" onClick={this.playerHit}>Hit</button>
+          <button className="modifierButtons" onClick={this.dealerHit}>Stick</button>
+          </div>
+        )
+      }
+
+      if(this.state.gameStarted) {
+        return (
+          <div>
+          <PlaceBet placeBet={this.placeBet} />
+
+          <p>You betted: {this.state.betAmount}</p>
+          </div>
+        )
+      }
       return (
         <div>
-        <Deck cards={this.state.dealerCards} />
-        <Deck cards={this.state.playerCards} />
+        <p id="appIntro">
+        Click 'play' to get started.
+        </p>
+        <button id="startButton" onClick={this.startGame}>
+        Play
+        </button>
         </div>
       )
     }
-
-    if(this.state.gameStarted) {
-      return (
-        <div>
-        <PlaceBet placeBet={this.placeBet} />
-
-        <p>You betted: {this.state.betAmount}</p>
-        </div>
-      )
-    }
-    return (
-      <div>
-      <p id="appIntro">
-      Click 'play' to get started.
-      </p>
-      <button id="startButton" onClick={this.startGame}>
-      Play
-      </button>
-      </div>
-    )
   }
-}
 
 
-export default Game;
+  export default Game;
